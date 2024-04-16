08f4d38e1003e6bd59f98c0a70a0130f86af061b
Adam <lv4mj1@gmail.com>
2024-04-16 21:36:27 +0100

    update receipt background

--- resources/views/pdf-receipt-view/payment-receipt.blade.php
+++ resources/views/pdf-receipt-view/payment-receipt.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <div class="bg-gray-50 dark:bg-gray-800 p-8 rounded-lg mt-20">
+    <div class=" dark:bg-gray-800 p-8 rounded-lg mt-20">
         <div class="flex justify-between items-center mb-8">
             <div>
                 <h1 class="font-bold text-xl text-gray-800 dark:text-gray-100 mb-4">Receipt</h1>
