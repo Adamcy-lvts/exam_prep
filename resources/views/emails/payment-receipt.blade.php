fdb6678ee1ef63311df42b6c7cb0bfba5397f540
Adam <lv4mj1@gmail.com>
2024-04-17 12:32:10 +0100

    update paymentReceipt style

--- resources/views/emails/payment-receipt.blade.php
+++ resources/views/emails/payment-receipt.blade.php
@@ -1,17 +1,24 @@
 <x-mail::message>
 # Payment Receipt
 
-Thank you for your payment.
+Thank you for your recent payment to {{ config('app.name') }}. Below are the details of your transaction:
 
-Here are the details of your subscription plan:
+@component('mail::panel')
+**Plan:** {{ $subsPlan }}  
+**Amount Paid:** ${{ formatNaira($payment->amount) }}  
+**Date:** {{ formatDate($receipt->payment_date) }}  
+**Payment Method:** {{ ucfirst($payment->method) }}
+@endcomponent
 
-- **Plan:** {{ $subsPlan }}
-- **Amount:** {{ formatNaira($payment->amount) }}
-- **Date:** {{ formatDate($receipt->payment_date)}}
+A PDF copy of your receipt has been attached to this email for your records.
 
-<x-mail::button :url="$urlToReceipt">
-Download Receipt
-</x-mail::button>
+@if(isset($urlToOnlineReceipt))
+    <x-mail::button :url="$urlToOnlineReceipt">
+        View Receipt
+    </x-mail::button>
+@endif
+
+If you have any questions or concerns regarding your payment, please contact our support team.
 
 Thanks,<br>
 {{ config('app.name') }}
