1fb19d48720740f3a07d4012c760d8f262fc99f1
Adamcy-lvts <lv4mj1@gmail.com>
2024-09-12 09:29:17 +0100

    update vite file

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                `resources/css/filament/agent/theme.css`
-                `resources/css/filament/user/theme.css`
+                `resources/css/filament/agent/theme.css`,
+                `resources/css/filament/user/theme.css`,
             ],
             refresh: true,
         }),
